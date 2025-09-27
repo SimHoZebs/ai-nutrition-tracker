ed40b2bd117b46eee35a3a032bcdf5fa72498d79
Patrick <patrickzambonie@gmail.com>
2025-09-27 11:40:42 -0400

    Remove pnpm files, we switchin to npm

--- frontend/src/components/card/card.tsx
+++ frontend/src/components/card/card.tsx
@@ -1,4 +1,4 @@
-import {ReactNode} from 'react';
+import type {ReactNode} from 'react';
 import styles from './card.module.css';
 import clsx from 'clsx';
 
--- frontend/src/components/nutrient-card/nutrient-card.tsx
+++ frontend/src/components/nutrient-card/nutrient-card.tsx
@@ -1,11 +1,14 @@
 import styles from './nutrient-card.module.css'
-import Card from '../card/card.tsx';
+import Card from '../card/card.tsx'
 
 export default function NutrientCard() {
   return (
     <Card className={styles.nutrientCard} variant='plain'>
       <h2>Calories</h2>
-      <p>100</p>
+      <div className={styles.counter}>
+        <p>100</p>
+
+      </div>
     </Card>
   )
 }
