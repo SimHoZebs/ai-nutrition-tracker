17b41a3cfd7e3fb1630515d3f10733c6a10389c0
Patrick <patrickzambonie@gmail.com>
2025-09-27 21:25:53 -0400

    Add util for http requests

--- frontend/src/util.ts
+++ frontend/src/util.ts
@@ -1,1 +1,16 @@
 export const baseUrl = 'http://localhost:8000'
+
+type HttpMethods = "GET" | "POST" | "PUT" | "DELETE"
+export async function handleRequest(method: HttpMethods, endpoint: string, jsonBody: unknown) {
+  const res = await fetch(baseUrl + endpoint, {
+    method,
+    body: JSON.stringify(jsonBody),
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    }
+  })
+
+  return await res.json()
+}
